d1c959c753f5138f70b83c041889efaec430ea4c
Nguyễn Đức <ducna1462@gmail.com>
2023-04-01 13:42:48 +0700

    a

--- js/script.js
+++ js/script.js
@@ -112,13 +112,14 @@ next_btn.onclick = () => {
 // getting questions and options from array
 function showQuetions(index) {
     const que_text = document.querySelector(".que_text");
-
+    let option_tag = '';
     //creating a new span and div tag for question and option and passing the value using array index
     let que_tag = '<span>' + questions[index].numb + ". " + questions[index].question + '</span>';
-    let option_tag = '<div class="option"><span>' + questions[index].options[0] + '</span></div>'
-        + '<div class="option"><span>' + questions[index].options[1] + '</span></div>'
-        + '<div class="option"><span>' + questions[index].options[2] + '</span></div>'
-        + '<div class="option"><span>' + questions[index].options[3] + '</span></div>';
+
+    for (i = 0; i < questions[index].options.length; i++) {
+        option_tag += '<div class="option"><span>' + questions[index].options[i] + '</span></div>'
+    }
+
     que_text.innerHTML = que_tag; //adding new span tag inside que_tag
     option_list.innerHTML = option_tag; //adding new div tag inside option_tag
 
